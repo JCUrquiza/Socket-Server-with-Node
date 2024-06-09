7d1cb3c43f113039ddc62cfa4b9189ef3c749879
Juan Carlos <gurquiza8@gmail.com>
2024-06-09 15:02:54 -0600

    Finished all methods of tickets

--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -3,7 +3,7 @@ import { Ticket } from '../../domain';
 
 export class TicketService {
 
-    private readonly tickets: Ticket[] = [
+    public readonly tickets: Ticket[] = [
         { id: UuidAdapter.v4(), number: 1, createdAt: new Date(), done: false },
         { id: UuidAdapter.v4(), number: 2, createdAt: new Date(), done: false },
         { id: UuidAdapter.v4(), number: 3, createdAt: new Date(), done: false },
@@ -12,19 +12,26 @@ export class TicketService {
         { id: UuidAdapter.v4(), number: 6, createdAt: new Date(), done: false },
     ];
 
+    private readonly workingOnTickets: Ticket[] = [];
+
+    
     public get pendingTickets(): Ticket[] {
         // return this.tickets.filter( ticket => ticket.done == false );
         return this.tickets.filter( ticket => !ticket.handleAtDesk );
     }
+    
+    public get lastWorkingOnTickets(): Ticket[] {
+        return this.workingOnTickets.splice(0, 4);
+    }
 
-    public lastTicketNumber(): number {
+    public get lastTicketNumber(): number {
         // const ticketsSize = this.tickets.length;
         // return this.tickets.filter( ticket => ticket.number == ticketsSize);
         return this.tickets.length > 0 ? this.tickets.at(-1)!.number : 0;
     }
 
     public createTicket() {
-        const ticket: Ticket = { id: UuidAdapter.v4(), number: this.lastTicketNumber() + 1, createdAt: new Date(), done: false };
+        const ticket: Ticket = { id: UuidAdapter.v4(), number: this.lastTicketNumber + 1, createdAt: new Date(), done: false };
         this.tickets.push(ticket);
         // TODO: WS
         return ticket;
@@ -36,6 +43,9 @@ export class TicketService {
 
         ticket.handleAtDesk = desk;
         ticket.handleAt = new Date();
+
+        // Agregar el ticket trabajando al arreglo para mostrarlo en pantalla:
+        this.workingOnTickets.unshift({ ...ticket });
 
         // TODO WS
         return { status: 'ok', ticket };
